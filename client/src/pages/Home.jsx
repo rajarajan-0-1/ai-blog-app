fad6b3e30bc1a8f5927bdc794ea77caf9aab7ebe
Rajarajan <rajarajanclg@gmail.com>
2025-11-27 15:04:13 +0530

    Correct NewsLetter component path for Vercel build

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
-import NewsLetter from '../components/NewsLetter'
 import Footer from '../components/Footer'
+import NewsLetter from '../components/Newsletter'
 
 const Home = () => {
   return (
